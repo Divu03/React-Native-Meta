0aad7e44a8427fe8df6f91fb9f11a88fa14b6e7c
Divu <divyeshkuchhadia@gmail.com>
2024-06-29 22:57:18 +0530

    Practicing the View and Text

---
+++ first-rn-component/components/Practice.js
@@ -0,0 +1,11 @@
+import {View,Text} from 'react-native'
+
+export default function Practice(){
+    return(
+        <>
+            <View>
+                <Text>Hi I am divyesh practicing the React native code</Text>
+            </View>
+        </>
+    );
+}
