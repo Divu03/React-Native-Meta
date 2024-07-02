eb3466f28d783ff2788bd345a987f6a7172998ec
Divu <divyeshkuchhadia@gmail.com>
2024-07-02 13:19:06 +0530

    Starting the excercise 1.4.10

--- first-rn-component/components/WelcomeScreen.js
+++ first-rn-component/components/WelcomeScreen.js
@@ -6,7 +6,7 @@ export default function WelcomeScreen() {
       <Text
         style={{
           padding: 40,
-          fontSize: 30,
+          fontSize: 50,
           color: '#EDEFEE',
           textAlign: 'center',
         }}>
@@ -14,7 +14,7 @@ export default function WelcomeScreen() {
       </Text>
       <Text
         style={{
-          fontSize: 24,
+          fontSize: 38,
           padding: 20,
           marginVertical: 8,
           color: '#EDEFEE',
@@ -22,7 +22,7 @@ export default function WelcomeScreen() {
         }}>
         Little Lemon is a charming neighborhood bistro that serves simple food
         and classic cocktails in a lively but casual environment. We would love
-        to hear your experience with us!
+        to hear more about your experience with us!
       </Text>
     </View>
   );
