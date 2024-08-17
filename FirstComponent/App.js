fb21de80af55b435921257979bef7fb5b220b774
Divu <divyeshkuchhadia@gmail.com>
2024-08-17 23:13:30 +0530

    starter code for excersice 2.1.4

--- FirstComponent/App.js
+++ FirstComponent/App.js
@@ -1,11 +1,29 @@
-import { View } from 'react-native';
+import * as React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
 
-import LLRHeader from './components/LLRHeader';
+import LittleLemonHeader from './components/LittleLemonHeader';
+import LittleLemonFooter from './components/LittleLemonFooter';
+import WelcomeScreen from './WelcomeScreen';
 
 export default function App() {
   return (
-    <View style={{ flex: 1, backgroundColor: '#495E57'}}>
-      <LLRHeader/>
-    </View>
+    <>
+      <View style={styles.container}>
+        <LittleLemonHeader />
+        <WelcomeScreen />
+      </View>
+      <View style={styles.footerContainer}>
+        <LittleLemonFooter />
+      </View>
+    </>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#333333',
+  },
+  footerContainer: { backgroundColor: '#333333' },
+});
+
