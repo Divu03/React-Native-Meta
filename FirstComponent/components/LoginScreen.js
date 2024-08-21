bf1fd8a2172266c3df5f514496332fe32b09360a
Divu <divyeshkuchhadia@gmail.com>
2024-08-21 18:22:46 +0530

    Excersice 2.4.4 completed

--- FirstComponent/components/LoginScreen.js
+++ FirstComponent/components/LoginScreen.js
@@ -1,11 +1,34 @@
-import { ScrollView, Text, StyleSheet} from 'react-native';
+import { ScrollView, Text, StyleSheet, TextInput, KeyboardAvoidingView, Platform} from 'react-native';
+import {useState} from 'react';
 
 export default function LoginScreen() {
+
+  const [email, onChangeEmail] = useState('');
+  const [password, onChangePassword] = useState('');
+
   return (
-    <ScrollView style={styles.container}>
-      <Text style={styles.headerText}>Welcome to Little Lemon</Text>
-      <Text style={styles.regularText}>Login to continue </Text>
-    </ScrollView>
+    <KeyboardAvoidingView 
+      style={styles.container}
+       behavior={Platform.OS === 'ios' ? 'padding' : 'height'} 
+    >
+      <ScrollView keyboardDismissMode="on-drag">
+        <Text style={styles.headerText}>Welcome to Little Lemon</Text>
+        <Text style={styles.regularText}>Login to continue </Text>
+        <TextInput
+          value={email}
+          onChangeText={onChangeEmail}
+          placeholder= "email"
+          style = {styles.inputBox}
+        />
+        <TextInput
+          value={password}
+          onChangeText={onChangePassword}
+          placeholder= "password"
+          style = {styles.inputBox}
+          secureTextEntry={true}
+        />
+      </ScrollView>
+    </KeyboardAvoidingView>
   );
 }
 
@@ -18,6 +41,15 @@ const styles = StyleSheet.create({
     fontSize: 30,
     color: '#EDEFEE',
     textAlign: 'center',
+  },
+  inputBox: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+    fontSize: 16,
+    borderColor: '#EDEFEE',
+    backgroundColor: '#EDEFEE'
   },
   regularText: {
     fontSize: 24,
