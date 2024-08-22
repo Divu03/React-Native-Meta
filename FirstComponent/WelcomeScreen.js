5d1c1b4456c8601806c199e032ffd2cae837040e
Divu <divyeshkuchhadia@gmail.com>
2024-08-22 21:53:53 +0530

    Excersice 3.2.8 completed

--- FirstComponent/WelcomeScreen.js
+++ FirstComponent/WelcomeScreen.js
@@ -1,20 +1,23 @@
-import { ScrollView, Text, StyleSheet, TextInput, Image, View } from 'react-native';
+import { ScrollView, View, Text, StyleSheet, Image } from 'react-native';
 
 export default function WelcomeScreen() {
   return (
     <ScrollView style={styles.container}>
       <View style={styles.headerWrapper}>
-      <Image 
-          source={require('./img/logo.png')} 
+        <Image
           style={styles.image}
-          resizeMode='contain'
+          source={require('./img/logo.png')}
+          resizeMode="cover"
+          accessible={true}
+          accessibilityLabel={'Little Lemon Logo'}
         />
-        <Text style={styles.headerText}>Welcome to Little Lemon</Text>
+
+        <Text style={styles.headerText}>Little Lemon</Text>
       </View>
       <Text style={styles.regularText}>
         Little Lemon is a charming neighborhood bistro that serves simple food
         and classic cocktails in a lively but casual environment. We would love
-        to hear more about your experience with us!
+        to hear your experience with us!
       </Text>
     </ScrollView>
   );
@@ -22,15 +25,18 @@ export default function WelcomeScreen() {
 
 const styles = StyleSheet.create({
   container: {
-    flex: .5,
+    flex: 1,
   },
-   headerWrapper: {
+  headerWrapper: {
     flexDirection: 'row',
     justifyContent: 'center',
     margin: 10,
   },
   headerText: {
-    padding: 40,
+    paddingRight: 10,
+    paddingLeft: 20,
+    paddingTop: 30,
+    paddingBottom: 10,
     fontSize: 30,
     color: '#EDEFEE',
     textAlign: 'center',
@@ -42,7 +48,7 @@ const styles = StyleSheet.create({
     color: '#EDEFEE',
     textAlign: 'center',
   },
-    image: {
+  image: {
     width: 100,
     height: 100,
     borderRadius: 20,
