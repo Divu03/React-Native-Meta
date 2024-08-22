6b1c0249584ea609370c75afcb76c95319724bd8
Divu <divyeshkuchhadia@gmail.com>
2024-08-22 21:26:19 +0530

    Excersice 3.1.4 completed

--- FirstComponent/components/LoginScreen.js
+++ FirstComponent/components/LoginScreen.js
@@ -1,32 +1,54 @@
-import { ScrollView, Text, StyleSheet, TextInput, KeyboardAvoidingView, Pressable, Platform} from 'react-native';
-import {useState} from 'react';
+import {
+  ScrollView,
+  Text,
+  StyleSheet,
+  TextInput,
+  KeyboardAvoidingView,
+  Platform,
+  Pressable
+} from 'react-native';
+import { useState } from 'react';
 
 export default function LoginScreen() {
-
   const [email, onChangeEmail] = useState('');
   const [password, onChangePassword] = useState('');
+  const [login, setLogin] = useState(false);
 
   return (
-    <KeyboardAvoidingView 
+    <KeyboardAvoidingView
       style={styles.container}
-       behavior={Platform.OS === 'ios' ? 'padding' : 'height'} 
-    >
+      behavior={Platform.OS === 'ios' ? 'padding' : 'height'}>
       <ScrollView keyboardDismissMode="on-drag">
         <Text style={styles.headerText}>Welcome to Little Lemon</Text>
-        <Text style={styles.regularText}>Login to continue </Text>
-        <TextInput
-          value={email}
-          onChangeText={onChangeEmail}
-          placeholder= "email"
-          style = {styles.inputBox}
-        />
-        <TextInput
-          value={password}
-          onChangeText={onChangePassword}
-          placeholder= "password"
-          style = {styles.inputBox}
-          secureTextEntry={true}
-        />
+        {!login && (
+          <>
+            <Text style={styles.regularText}>Login to continue </Text>
+            <TextInput
+              value={email}
+              onChangeText={onChangeEmail}
+              placeholder=" Email "
+              placeholderTextColor="#888888"
+              style={styles.inputBox}
+              keyboardType="email-address"
+            />
+            <TextInput
+              value={password}
+              onChangeText={onChangePassword}
+              style={styles.inputBox}
+              placeholder=" Password "
+              placeholderTextColor="#888888"
+              secureTextEntry={true}
+            />
+            <Pressable style={styles.loginButton} onPress={()=> {setLogin(true)}}>
+              <Text style={styles.buttonText}>Login</Text>
+            </Pressable>
+          </>
+        )}
+        {login && (
+          <>
+            <Text style={styles.headerText}> You are logged in!  </Text>
+          </>
+        )}
       </ScrollView>
     </KeyboardAvoidingView>
   );
@@ -49,13 +71,28 @@ const styles = StyleSheet.create({
     padding: 10,
     fontSize: 16,
     borderColor: '#EDEFEE',
-    backgroundColor: '#EDEFEE'
+    backgroundColor: '#EDEFEE',
   },
   regularText: {
     fontSize: 24,
     padding: 20,
     marginVertical: 8,
     color: '#EDEFEE',
+    textAlign: 'center',
+  },
+  loginButton:{
+    fontSize: 22,
+    padding: 10,
+    marginVertical: 8,
+    margin: 100,
+    backgroundColor: '#EE9972',
+    borderColor: '#EE9972',
+    borderWidth: 2,
+    borderRadius: 50,
+  },
+  buttonText:{
+    fontSize: 25,
+    color: '#000000',
     textAlign: 'center',
   },
 });
